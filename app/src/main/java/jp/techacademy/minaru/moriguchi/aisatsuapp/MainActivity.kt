7e87430e1af96a77eb6b5437fe20c2d86e09bbe0
MinaruM <minaru.moriguchi@gmail.com>
2019-10-07 08:47:20 -0400

    Initial commit

---
+++ app/src/main/java/jp/techacademy/minaru/moriguchi/aisatsuapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package jp.techacademy.minaru.moriguchi.aisatsuapp
+
+import android.app.TimePickerDialog
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+abstract class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener{
+            showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+                this,
+                TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                    Log.d("UI_PARTS", "$hour:$minute")
+                },
+                13, 0, true)
+        timePickerDialog.show()
+    }
+}
